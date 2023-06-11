10fd999f035a01237295516bc8822fa081a3bea4
Belén Albeza <belen@hey.com>
2023-06-11 18:45:33 +0200

    load pokemon data from api

--- src/app/pokemon/[id]/page.tsx
+++ src/app/pokemon/[id]/page.tsx
@@ -1,5 +1,4 @@
 import { FC } from "react";
-import Image from "next/image";
 
 import styles from "./page.module.css";
 
@@ -9,17 +8,43 @@ type PokemonDetails = {
   types: string[];
 };
 
-const getData: () => Promise<PokemonDetails> = async () => {
+type ApiResponse = {
+  name: string;
+  sprites: {
+    front_default: string;
+  };
+  types: {
+    type: {
+      name: string;
+    };
+  }[];
+};
+
+const getData: (id: string) => Promise<PokemonDetails> = async (id: string) => {
+  const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+
+  if (!response.ok) {
+    throw new Error(`Failed to fetch API data for pokemon with ID: ${id}`);
+  }
+
+  const data = (await response.json()) as ApiResponse;
+
   return {
-    name: "Pikachu",
-    image:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png",
-    types: ["electric"],
+    name: data.name,
+    image: data.sprites.front_default,
+    types: data.types.map((x) => x.type.name),
   };
 };
 
-const PokemonPage: FC = async () => {
-  const { name, image, types } = await getData();
+type PageProps = {
+  params: {
+    id: string;
+  };
+};
+
+const PokemonPage: FC<PageProps> = async ({ params }: PageProps) => {
+  const { id } = params;
+  const { name, image, types } = await getData(id);
 
   return (
     <article className={styles.page}>
